587bb704664d680d8c780460ed9b134ba365975a
wtscott@gmail.com <wtscott@gmail.com>
2018-04-22 01:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
+
 var cart = [];
 
 function getCart() {
- return cart;
-}
-
-function setCart(c) {
+  return cart;}
+function setCart© {
   cart = c;
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(name) {
+  var price = Math.floor(Math.random() * 100) + 1;
+  var item = {itemName: name, itemPrice: price};
+  cart.push(item);
+  return "${name} has been added to your cart."
 }
 
-function viewCart() {
-  // write your code here
+function viewCart(){
+
 }
 
 function total() {
-  // write your code here
+
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
